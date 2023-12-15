49b94ab4d7fc3466433f7f33ed689977e4cb661c
Udayakumar200 <udayabalu7@gmail.com>
2023-12-15 17:33:49 +0530

    task 1 q.no 1

---
+++ forloop/src/Main.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+public class Main {
+    public  static  void main(String[]agrs){
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt();
+        int m = obj.nextInt();
+        for (int i =n;i<=m;i++){
+            System.out.println( i);
+        }
+    }
+}
