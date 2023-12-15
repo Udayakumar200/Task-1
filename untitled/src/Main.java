fd55774e5e18b3be644c971b0a09d97495815211
Udayakumar200 <udayabalu7@gmail.com>
2023-12-15 16:43:37 +0530

    this is my first file

---
+++ untitled/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]args){
+        Scanner obj =new Scanner(System.in);
+        int u = obj.nextInt();
+        int m = obj.nextInt();
+        for(int i=u;i<=m;i++){
+            System.out.println(i);
+        }
+    }
+}
